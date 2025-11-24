afcf5e43236830a528da64f3d1c44700fb3e4149
aby <aby.xavier1@gmail.com>
2025-11-24 17:07:02 +0530

    Intial Commit and setup UI

---
+++ ooham-frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen ">
+      <div>This is a page</div>
+      <h1>d</h1>
+    </div>
+  );
+}
